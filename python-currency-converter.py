19697d80ad28041ca9c898ba29012ff0e620f6b9
s-kaczmarek <skaczmarek88@gmail.com>
2020-01-13 23:24:07 +0100

    update

--- python-currency-converter.py
+++ python-currency-converter.py
@@ -3,12 +3,11 @@
 import sys
 import requests
 
-amount = sys.argv[1]
-base_currency = sys.argv[2]
-target_currency = sys.argv[3]
-
 def help():
     print("Python Currency Converter help")
+    print("Usage:")
+    print("    <amount> <base currency> <target currency> - example: python-currency-converter 10 USD PLN")
+    print("    -h - print this help message")
 
 def arguments_validator():
     if (sys.argv[1].isdigit()) and (len(sys.argv[2]) == 3) and (len(sys.argv[3]) == 3):
@@ -17,7 +16,11 @@ def arguments_validator():
         return False
 
 def convert_currency():
+    amount = sys.argv[1]
+    base_currency = sys.argv[2]
+    target_currency = sys.argv[3]
 
+    # this function converts only from foreign currency to PLN
     def nbp_converter():
         response = requests.get("https://api.nbp.pl/api/exchangerates/rates/a/{}/?format=json".format(base_currency))
 
@@ -26,14 +29,12 @@ def convert_currency():
             last_rate = rates[0]
 
         mid_price = last_rate["mid"]
-        print(mid_price)
+        var = mid_price * float(amount)
+        print("{} {} {} {} {}".format(amount, base_currency.upper(), "=", var, "PLN"))
 
     nbp_converter()
 
-
-
-
-
+# Flow
 
 if len(sys.argv) == 4:
     if arguments_validator():
